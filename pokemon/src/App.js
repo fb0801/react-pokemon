0df324cf5bd1f7d3ac12b1f22d8c67749890c8cc
fb0801 <fbhatti0801@gmail.com>
2023-08-04 15:44:50 +0100

    gotta catch them all 4

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -3,9 +3,32 @@ import PokemonList from "./PokemonList";
 import axios from 'axios';
 
 function App() {
-  const  [pokemon, setPokemon] = useState(['Drippy', 'Gengar'])
+  const  [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon/')
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    let cancel
+    setLoading(true)
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+    setPokemon(res.data.results.map(p => p.name))
+  })
+
+  return () => cancel.cancel()
+
+  }, [currentPageUrl])
+  
+if (loading) return 'loading...'
   return (
    
+    
     <PokemonList pokemon={pokemon} />
 
   );
